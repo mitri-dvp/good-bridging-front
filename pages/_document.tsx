b91e1d1708e946c514efc0b1aba29630ab4f31e7
mitri-dvp <mitri.dvp@gmail.com>
2021-10-27 12:26:24 -0400

    Custom fonts added

---
+++ pages/_document.tsx
@@ -0,0 +1,78 @@
+import Document, { Html, Head, Main, NextScript } from 'next/document'
+
+class MyDocument extends Document {
+  render() {
+    return (
+      <Html>
+        <Head>
+        <link
+            rel="preload"
+            href="/fonts/Gilroy/Gilroy-Light.ttf"
+            as="font"
+            crossOrigin=""
+        />
+        <link
+            rel="preload"
+            href="/fonts/Gilroy/Gilroy-Regular.ttf"
+            as="font"
+            crossOrigin=""
+        />
+        <link
+            rel="preload"
+            href="/fonts/Gilroy/Gilroy-Medium.ttf"
+            as="font"
+            crossOrigin=""
+        />
+        <link
+            rel="preload"
+            href="/fonts/Gilroy/Gilroy-Bold.ttf"
+            as="font"
+            crossOrigin=""
+        />
+        <link
+            rel="preload"
+            href="/fonts/Gilroy/Gilroy-Heavy.ttf"
+            as="font"
+            crossOrigin=""
+        />
+        <link
+            rel="preload"
+            href="/fonts/Roboto/Roboto-Light.ttf"
+            as="font"
+            crossOrigin=""
+        />
+        <link
+            rel="preload"
+            href="/fonts/Roboto/Roboto-Regular.ttf"
+            as="font"
+            crossOrigin=""
+        />
+        <link
+            rel="preload"
+            href="/fonts/Roboto/Roboto-Medium.ttf"
+            as="font"
+            crossOrigin=""
+        />
+        <link
+            rel="preload"
+            href="/fonts/Roboto/Roboto-Bold.ttf"
+            as="font"
+            crossOrigin=""
+        />
+        <link
+            rel="preload"
+            href="/fonts/Roboto/Roboto-Heavy.ttf"
+            as="font"
+            crossOrigin=""
+        />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    )
+  }
+}
+
+export default MyDocument
