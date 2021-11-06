098aedb2e05817c1c45bac1d334edf345fda0f6b
mitri-dvp <mitri.dvp@gmail.com>
2021-11-05 21:09:14 -0400

    Coming soon responsive design, blog page added

--- components/Header.tsx
+++ components/Header.tsx
@@ -10,58 +10,72 @@ export default function Header() {
   const [showMobileNav, setShowmobileNav] = useState(false)
 
   if(!isHome) return (
-    <header className={styles.header}>
-      <nav className={styles.desktop_nav}>
-        <Link href="/">
-          <a className={styles.logo}>
+    <>
+      <header className={`${styles.header} ${styles.desktop}`}>
+        <nav>
+          <a href="#" className={styles.logo}>
             <img src="/assets/png/goob_bridging_brandmark_color.png" alt="goob_bridging_brandmark_color" />
             <div>Good <span>Bridging</span></div>
           </a>
-        </Link>
-        <Link href="/#about"><a>About</a></Link>
-        <Link href="/#blog"><a>Blog</a></Link>
-        <Link href="/#ecosystem"><a>Ecosystem</a></Link>
-        <Link href="/#calendar"><a>Calendar</a></Link>
-        <Link href="/nft"><a>NFTs</a></Link>
-      </nav>
-      <nav className={styles.mobile_nav}>
-        Mobile
-      </nav>
-    </header>
+          <Link href="/#about"><a>About</a></Link>
+          {/* <Link href="/blog"><a>Blog</a></Link> */}
+          <Link href="/#ecosystem"><a>Ecosystem</a></Link>
+          <Link href="/#calendar"><a>Calendar</a></Link>
+          <Link href="/nft"><a>NFTs</a></Link>
+        </nav>
+      </header>
+      <header className={`${styles.header} ${styles.mobile}`}>
+        <a href="#" className={styles.logo}>
+          <img src="/assets/png/goob_bridging_brandmark_color.png" alt="goob_bridging_brandmark_color" />
+        </a>
+        <button className={styles.mobile_button}>
+          <svg  onClick={() => setShowmobileNav(!showMobileNav)} xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" viewBox="0 0 16 16">
+            <path fillRule="evenodd" d="M2.5 12a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5z"/>
+          </svg>
+        </button>
+        <nav className={`${showMobileNav ? styles.active : ''}`}>
+          <Link href="/#about"><a>About</a></Link>
+          {/* <Link href="/blog"><a>Blog</a></Link> */}
+          <Link href="/#ecosystem"><a>Ecosystem</a></Link>
+          <Link href="/#calendar"><a>Calendar</a></Link>
+          <Link href="/nft"><a>NFTs</a></Link>
+        </nav>
+      </header>
+    </>
   )
 
   return (
     <>
-    <header className={`${styles.header} ${styles.desktop}`}>
-      <nav>
+      <header className={`${styles.header} ${styles.desktop}`}>
+        <nav>
+          <a href="#" className={styles.logo}>
+            <img src="/assets/png/goob_bridging_brandmark_color.png" alt="goob_bridging_brandmark_color" />
+            <div>Good <span>Bridging</span></div>
+          </a>
+          <a href="#about">About</a>
+          {/* <Link href="/blog"><a>Blog</a></Link> */}
+          <a href="#ecosystem">Ecosystem</a>
+          <a href="#calendar">Calendar</a>
+          <Link href="/nft"><a>NFTs</a></Link>
+        </nav>
+      </header>
+      <header className={`${styles.header} ${styles.mobile}`}>
         <a href="#" className={styles.logo}>
           <img src="/assets/png/goob_bridging_brandmark_color.png" alt="goob_bridging_brandmark_color" />
-          <div>Good <span>Bridging</span></div>
         </a>
-        <a href="#about">About</a>
-        <a href="#blog">Blog</a>
-        <a href="#ecosystem">Ecosystem</a>
-        <a href="#calendar">Calendar</a>
-        <Link href="/nft"><a>NFTs</a></Link>
-      </nav>
-    </header>
-    <header className={`${styles.header} ${styles.mobile}`}>
-      <a href="#" className={styles.logo}>
-        <img src="/assets/png/goob_bridging_brandmark_color.png" alt="goob_bridging_brandmark_color" />
-      </a>
-      <button className={styles.mobile_button}>
-        <svg  onClick={() => setShowmobileNav(!showMobileNav)} xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" viewBox="0 0 16 16">
-          <path fillRule="evenodd" d="M2.5 12a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5z"/>
-        </svg>
-      </button>
-      <nav className={`${showMobileNav ? styles.active : ''}`}>
-        <a href="#about">About</a>
-        <a href="#blog">Blog</a>
-        <a href="#ecosystem">Ecosystem</a>
-        <a href="#calendar">Calendar</a>
-        <Link href="/nft"><a>NFTs</a></Link>
-      </nav>
-    </header>
+        <button className={styles.mobile_button}>
+          <svg  onClick={() => setShowmobileNav(!showMobileNav)} xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" viewBox="0 0 16 16">
+            <path fillRule="evenodd" d="M2.5 12a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5z"/>
+          </svg>
+        </button>
+        <nav className={`${showMobileNav ? styles.active : ''}`}>
+          <a href="#about">About</a>
+          {/* <Link href="/blog"><a>Blog</a></Link> */}
+          <a href="#ecosystem">Ecosystem</a>
+          <a href="#calendar">Calendar</a>
+          <Link href="/nft"><a>NFTs</a></Link>
+        </nav>
+      </header>
     </>
   )
 }
