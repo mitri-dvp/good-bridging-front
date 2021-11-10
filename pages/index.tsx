a173a65e884a840d1d5992283124a67d6ca98309
mitri-dvp <mitri.dvp@gmail.com>
2021-11-10 13:41:39 -0500

    Swiper disabled

--- pages/index.tsx
+++ pages/index.tsx
@@ -85,7 +85,7 @@ const Home: NextPage<{partners: Partner[]}> = ({ partners }) => {
                   <path d="m12.14 8.753-5.482 4.796c-.646.566-1.658.106-1.658-.753V3.204a1 1 0 0 1 1.659-.753l5.48 4.796a1 1 0 0 1 0 1.506z"/>
                 </svg>
               </button>
-              <Swiper
+              {/* <Swiper
                 modules={[Navigation, A11y]}
                 spaceBetween={0}
                 slidesPerView={2}
@@ -108,7 +108,7 @@ const Home: NextPage<{partners: Partner[]}> = ({ partners }) => {
                     </a>
                   </SwiperSlide>
                 ))}
-              </Swiper>
+              </Swiper> */}
               </div>
             </div>
           </div>
