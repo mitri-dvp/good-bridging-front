33cf39aec30bcfa613f7bfe48cce031b2d89b1a8
mitri-dvp <mitri.dvp@gmail.com>
2021-11-10 13:44:38 -0500

    Swiper added with one slide

--- pages/index.tsx
+++ pages/index.tsx
@@ -85,7 +85,7 @@ const Home: NextPage<{partners: Partner[]}> = ({ partners }) => {
                   <path d="m12.14 8.753-5.482 4.796c-.646.566-1.658.106-1.658-.753V3.204a1 1 0 0 1 1.659-.753l5.48 4.796a1 1 0 0 1 0 1.506z"/>
                 </svg>
               </button>
-              {/* <Swiper
+              <Swiper
                 modules={[Navigation, A11y]}
                 spaceBetween={0}
                 slidesPerView={2}
@@ -100,15 +100,18 @@ const Home: NextPage<{partners: Partner[]}> = ({ partners }) => {
                   },
                 }}
               >
-                {partners.map((partner, i) => (
+                <SwiperSlide>
+                  <div>test</div>
+                </SwiperSlide>
+                {/* {partners.map((partner, i) => (
                   <SwiperSlide key={partner.screen_name}>
                     <a href={`https://twitter.com/${partner.screen_name}`} target="_good_bridging" className={styles.partner}>
                       <img src={partner.profile_pic} alt={partner.screen_name} />
                       <span>@{partner.screen_name}</span>
                     </a>
                   </SwiperSlide>
-                ))}
-              </Swiper> */}
+                ))} */}
+              </Swiper>
               </div>
             </div>
           </div>
